148700ef8c779f206b39127213d98252395c8dce
ldesou2 <elodiesu1126@gmail.com>
2026-01-18 09:19:05 -0500

    getting stuff from elodie branch

---
+++ frontend/src/components/post.jsx
@@ -0,0 +1,30 @@
+// src/webpages/post.jsx
+
+import '../styles/post-style.css';
+
+const Post = ({post}) => {
+    const {title, description, picture, tags} = post;
+
+
+  return (
+    <div className='post-card'
+    style={{ backgroundImage: `url(${picture})` }}
+    >
+        <div className='post-content'>
+            <h2 className="post-title">{title}</h2>
+            <p className="post-description">{description}</p>
+        
+             <div className="post-tags">
+            {tags && tags.map((tag, index) => (
+                <span key={index} className="tag">
+                #{tag}
+                </span>
+          ))}
+        </div>
+        </div>
+    </div>
+
+  );
+
+}
+export default Post;
